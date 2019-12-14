94b7f268cc16b034166829c467afc22964f08a98
Lucas <lucas_ssilva@outlook.com>
2019-12-13 22:15:07 -0300

    inicio

---
+++ polimorfismo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #{0} data:",i);
+                Console.Write("Outsourced (y/n)? ");
+                char outsourced = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if (outsourced == 'Y' || outsourced == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additional = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    list.Add(new OutSourcedEmployee(name, hours, valuePerHour, additional));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj.Name + " - $ " + obj.Payment().ToString("N2", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
